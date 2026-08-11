7ab752ddf2ddd86bc3875a0ee02e16ffb1cd8d6c
BoilingNoob <BoilingNoob@github.com>
2026-08-11 18:56:57 -0400

    -added scipy -added curve fitting for normal dist -added finding best result -plotting derivative

--- plotting.py
+++ plotting.py
@@ -3,21 +3,34 @@ import numpy as np
 import sympy as sy
 import random as rd
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from scipy.differentiate import derivative
 
 result_df = pd.read_csv("portion_sim_results.csv")
 
 print_df = result_df[["portion_param","median_payout"]].copy()
 
+def normal_dist_func(x,mu,sigma,a):
+    return a*np.exp(-((x-mu)**2)/(2*sigma**2))
 
 
-print(print_df.to_string())
+initial_guesses = [1,1,1]
 
-print_df["slope"]=(print_df["median_payout"] - print_df["median_payout"].shift(1)) / (print_df["portion_param"] - print_df["portion_param"].shift(1))
-print_df["slope_abs"] = print_df["slope"].abs()
+popt, pcov = curve_fit(normal_dist_func, print_df["portion_param"], print_df["median_payout"], p0=initial_guesses)
 
-print_df.plot(x="portion_param",y=["median_payout","slope"],title="Median Payout vs Portion Size",xlabel="Portion Size",ylabel="Median Payout",grid=True,legend=False)
+my_derivative_y = derivative(normal_dist_func, print_df["portion_param"], args=tuple(popt),preserve_shape=True).df #.values()
 
-best_criteria = print_df.sort_values(by=["slope_abs"],ascending=[True]).head(1)
-print(best_criteria.to_string())
+functionilzed = pd.DataFrame({"portion_param":print_df["portion_param"],"median_payout":normal_dist_func(print_df["portion_param"], *popt)})
+best_one = functionilzed.sort_values(by="median_payout",ascending=False).head(1)
+
+print("best value:")
+print(best_one.to_string())
+
+
+plt.scatter(print_df["portion_param"], print_df["median_payout"], color='red', label='Noisy Data')
+plt.plot(print_df["portion_param"], normal_dist_func(print_df["portion_param"], *popt), color='blue', label='Fitted Curve', linewidth=2)
+plt.plot(print_df["portion_param"], my_derivative_y, color='green', label='Fitted Curve', linewidth=2)
+plt.plot(best_one["portion_param"], best_one["median_payout"], marker='o', markersize=10, color='orange', label='Best Point')
+
 
 plt.show()
